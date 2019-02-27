f67006ca2d227d4f5ff08c86082db52836953c1b
Kwok Hiu Fung <e1652308@cmaisonneuve.qc.ca>
2019-02-27 10:36:14 -0500

    Portfolio V3 - Les sections jeux, 3d, web

---
+++ js/autoExecutante.js
@@ -0,0 +1,51 @@
+(function() {
+	"use strict";
+
+var menu = document.getElementById("menu");
+var logo = document.getElementById("logo");
+
+var btnMenuBurger = document.getElementById("chkBoutonBurger");
+var btnMenuMobile = document.getElementById("btnMenuMobile");
+
+//var btnMenuBurger = document.getElementById("chkBoutonBurger");
+//var btnMenuMobile = document.getElementById("btnMenuMobile");
+
+/*
+ *===================================================
+ * Taille de la bar de menu =========================
+ *===================================================
+*/
+window.onscroll = function() {
+	scrollFonction()
+};
+
+function scrollFonction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    menu.style.padding = "0em";
+    logo.style.height = "55px";
+  } else {
+    menu.style.padding = "1em";
+    logo.style.height = "70px";
+  }
+}
+
+/*
+ *===================================================
+ * Le menu burger ===================================
+ *===================================================
+*/
+
+btnMenuBurger.addEventListener("click", activeMenuBurger);
+
+function activeMenuBurger(){
+  if (btnMenuBurger.checked == true){
+    btnMenuMobile.style.transform = 'translateX(' + 0 + '%)';
+  } else if (btnMenuBurger.checked == false){
+    btnMenuMobile.style.transform = 'translateX(' + 100 + '%)';
+  }
+
+}
+
+})();
+
+
